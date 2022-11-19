011539fbe49f605ab96c36bd4718b1ec34e2dad5
VETRIVELR84422 <118591459+VETRIVELR84422@users.noreply.github.com>
2022-11-19 13:19:43 +0530

    Delete Assignments/Vetrivel/Assignment-3 Vetrivel directory

--- Assignments/Vetrivel/Assignment-3 Vetrivel/sqlite_db_setup.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('person_database.db')
-print("Opened db successfully")
-
-conn.execute('CREATE TABLE person (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-print("Table Created")
-
-conn.close()
